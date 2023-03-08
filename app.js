43abaacaab5e64019262ff4d38013296a009db2a
nyaongnyaooong <luckyyou123@naver.com>
2023-03-08 18:55:42 +0900

    addRouter

--- app.js
+++ app.js
@@ -29,6 +29,26 @@ app.get('/main', (req, res) => {
 });
 
 
+app.get('/board', (req, res) => {
+  // console.log("123");
+  // const filePath = path.join(__dirname, 'html', 'main.ejs')
+  res.sendFile(__dirname + '/public/main.html');
+  // res.sendFile(__dirname + '/index.html');
+});
+
+//admin 페이지
+app.get("/:htmlFileName", (req, res) => {
+  try {
+  const { htmlFileName } = req.params;
+  const htmlFileFullDir = __dirname + "/public/" + htmlFileName + ".html"
+  console.log(req.params.htmlFileName);
+  res.sendFile(htmlFileFullDir);
+  } catch (err) {
+    console.log(1);
+    console.error(err);
+  }
+});
+
 app.listen(8080, () => {
   console.log("listening on 8080")
 });
