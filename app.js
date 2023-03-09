362c6f8dc2124b4796d970811a03ef0c625632e4
nyaongnyaooong <luckyyou123@naver.com>
2023-03-09 23:19:22 +0900

    addDB

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
 const fs = require('fs');
 
 //express 모듈에 body-parser내장
-//const bodyParser = require('body-parser');
 app.use(express.urlencoded({ extended : true }));
 
-// const methodOverride = require('method-override');
-// app.use(methodOverride('_method'));
+//method-override for RESTFUL API
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
+//dotenv
+require('dotenv').config()
 
-//const { Script } = require('vm');
-app.set('view engine', 'html');
+// const { Script } = require('vm');
+// app.set('view engine', 'html');
  
-const mainHTML = fs.readFileSync(__dirname + "/html/main.ejs", "utf8");
-
-
+//Static File
 app.use(express.static("public"));
+
+//MongoDB Atlas Setting
+const dbURL = "mongodb+srv://" + process.env.DB_ID + ":" + process.env.DB_PW + process.env.DB_URL;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+let db;
+MongoClient.connect(dbURL, (err, result) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  db = result.db("project1");
+  console.log('DB connected.');
+
+  app.listen(8080, () => {
+    console.log("listening on 8080");
+  });
+
+});
+
+
+
+
 
 
 // Main Router
@@ -28,27 +51,45 @@ app.get('/main', (req, res) => {
   // res.sendFile(__dirname + '/index.html');
 });
 
-
 app.get('/board', (req, res) => {
   // console.log("123");
   // const filePath = path.join(__dirname, 'html', 'main.ejs')
-  res.sendFile(__dirname + '/public/main.html');
+  res.sendFile(__dirname + '/public/board.html');
   // res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/board/index', async (req, res) => {
+  const result = await db.collection("board").find().toArray();
+  console.log(result);
+  res.send(result);
+});
+
+
+
 //admin 페이지
-app.get("/:htmlFileName", (req, res) => {
+app.get("/:htmlFileName", async(req, res, next) => {
   try {
-  const { htmlFileName } = req.params;
-  const htmlFileFullDir = __dirname + "/public/" + htmlFileName + ".html"
-  console.log(req.params.htmlFileName);
-  res.sendFile(htmlFileFullDir);
+    const { htmlFileName } = req.params;
+    const htmlFileFullDir = __dirname + "/public/" + htmlFileName + ".html";
+    console.log(htmlFileName);
+    res.sendFile(htmlFileFullDir);
   } catch (err) {
     console.log(1);
     console.error(err);
   }
+  
 });
 
-app.listen(8080, () => {
-  console.log("listening on 8080")
+//404 Middleware
+app.use((req, res, next) => { // 404 처리 부분
+  console.log("404");
+  res.status(404).send('일치하는 주소가 없습니다!');
 });
+
+//Error Middleware
+app.use((err, req, res, next) => { // 에러 처리 부분
+  console.error(err.stack); // 에러 메시지 표시
+  res.status(500).send('서버 에러!'); // 500 상태 표시 후 에러 메시지 전송
+});
+
+
